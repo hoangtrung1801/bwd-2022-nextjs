21129c161239457cfadbb30e6761dfeee3790f58
TuanVo3423 <tuanvanvo2003@gmail.com>
2022-09-05 18:57:17 +0700

    fixed donate section at home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/buttons/Button";
 import CarouselBanner from "@/components/CarouselBanner";
 import CarouselProducts from "@/components/CarouselProducts";
 import CommonProducts from "@/components/CommonProducts";
@@ -6,6 +7,7 @@ import Layout from "@/components/layout/Layout";
 import ButtonLink from "@/components/links/ButtonLink";
 import useProducts from "@/lib/hooks/useProducts";
 import { motion } from "framer-motion";
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
@@ -62,9 +64,53 @@ const HomePage = () => {
                 <div>
                     <CommonProducts products={products} />
                 </div>
-                <div>
-                    <div className="h-80 w-full bg-[url(https://bwd2022.vercel.app/assets/bg6.jpg)] bg-cover bg-fixed bg-center bg-no-repeat text-white">
-                        <div className="flex h-full w-full flex-row items-center justify-center">
+                <div
+                    className="h-full w-full bg-cover bg-fixed bg-center bg-no-repeat text-white"
+                    style={{
+                        backgroundImage: `url(https://bwd2022.vercel.app/assets/bg6.jpg)`,
+                        height: "500px",
+                    }}
+                >
+                    <div className="flex h-full w-full flex-row items-center justify-center">
+                        <motion.div
+                            initial={{ opacity: 0, y: 200, x: 0 }}
+                            whileInView={{ opacity: 1, y: 0, x: 0 }}
+                            viewport={{ once: true }}
+                            transition={{
+                                type: "spring",
+                                duration: 1.5,
+                                bounce: 0.3,
+                            }}
+                            className="text-center"
+                        >
+                            <motion.p
+                                initial={{ opacity: 0, y: 200, x: 0 }}
+                                whileInView={{ opacity: 1, y: 0, x: 0 }}
+                                viewport={{ once: true }}
+                                transition={{
+                                    type: "spring",
+                                    duration: 1.5,
+                                    bounce: 0.3,
+                                }}
+                                className="mb-10 text-5xl font-bold"
+                            >
+                                Cùng chung tay quyên góp bảo vệ môi trường
+                            </motion.p>
+                            <motion.p
+                                initial={{ opacity: 0, y: 200, x: 0 }}
+                                whileInView={{ opacity: 1, y: 0, x: 0 }}
+                                viewport={{ once: true }}
+                                transition={{
+                                    type: "spring",
+                                    duration: 1.5,
+                                    bounce: 0.3,
+                                }}
+                                className="mx-auto w-3/4 text-lg"
+                            >
+                                Giúp đỡ lẫn nhau thực hiện các nhu cầu cơ bản
+                                của sự sống còn của con người, đấu tranh bảo tồn
+                                các nguồn tài nguyên thiên nhiên và chung tay.
+                            </motion.p>
                             <motion.div
                                 initial={{ opacity: 0, y: 200, x: 0 }}
                                 whileInView={{ opacity: 1, y: 0, x: 0 }}
@@ -74,51 +120,22 @@ const HomePage = () => {
                                     duration: 1.5,
                                     bounce: 0.3,
                                 }}
-                                className="text-center"
                             >
-                                <motion.p
-                                    initial={{ opacity: 0, y: 200, x: 0 }}
-                                    whileInView={{ opacity: 1, y: 0, x: 0 }}
-                                    viewport={{ once: true }}
-                                    transition={{
-                                        type: "spring",
-                                        duration: 1.5,
-                                        bounce: 0.3,
-                                    }}
-                                    className="mb-10 text-5xl font-bold"
-                                >
-                                    Cùng chung tay quyên góp bảo vệ môi trường
-                                </motion.p>
-                                <motion.p
-                                    initial={{ opacity: 0, y: 200, x: 0 }}
-                                    whileInView={{ opacity: 1, y: 0, x: 0 }}
-                                    viewport={{ once: true }}
-                                    transition={{
-                                        type: "spring",
-                                        duration: 1.5,
-                                        bounce: 0.3,
-                                    }}
-                                    className="mx-auto w-3/4 text-lg"
-                                >
-                                    Giúp đỡ lẫn nhau thực hiện các nhu cầu cơ
-                                    bản của sự sống còn của con người, đấu tranh
-                                    bảo tồn các nguồn tài nguyên thiên nhiên và
-                                    chung tay.
-                                </motion.p>
-                                <motion.div
-                                    initial={{ opacity: 0, y: 200, x: 0 }}
-                                    whileInView={{ opacity: 1, y: 0, x: 0 }}
-                                    viewport={{ once: true }}
-                                    transition={{
-                                        type: "spring",
-                                        duration: 1.5,
-                                        bounce: 0.3,
-                                    }}
-                                >
-                                    <ButtonLink href="#">Quyên góp</ButtonLink>
-                                </motion.div>
+                                <Link href="/donate">
+                                    <Button
+                                        style={{
+                                            backgroundImage:
+                                                "linear-gradient(to right, #232526 0%, #414345 51%, #232526 100%)",
+                                            transition: "0.5s",
+                                            backgroundSize: "200% auto",
+                                        }}
+                                        className="transition[0.5s] h-auto w-[200px] min-w-[200px] bg-black py-4 px-8 text-white hover:bg-right md:mt-10"
+                                    >
+                                        QUYÊN GÓP
+                                    </Button>
+                                </Link>
                             </motion.div>
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
             </main>
