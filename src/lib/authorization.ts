72d8264441eebf0d11f5623e268ba160393c6f2c
Hoang Trung <hoangtrung1801.2003@gmail.com>
2022-09-07 11:40:30 +0700

    feat: 🎸 sign up feature

--- src/lib/authorization.ts
+++ src/lib/authorization.ts
@@ -3,6 +3,7 @@ import fetcher from "@/lib/fetcher";
 
 const LOGIN_API_URL = `${API_SERVER_URL}/auth/login`;
 const LOGOUT_API_URL = `${API_SERVER_URL}/auth/logout`;
+const SIGNUP_API_URL = `${API_SERVER_URL}/auth/signup`;
 
 export const loginUser = async (email: string, password: string) => {
     const response = await fetcher(LOGIN_API_URL, {
@@ -10,7 +11,6 @@ export const loginUser = async (email: string, password: string) => {
         headers: {
             "Content-Type": "application/json",
         },
-        credentials: "include",
         body: JSON.stringify({
             email,
             password,
@@ -22,7 +22,37 @@ export const loginUser = async (email: string, password: string) => {
 export const logoutUser = async () => {
     const response = await fetcher(LOGOUT_API_URL, {
         method: "POST",
-        credentials: "include",
     });
     return response.status === "success";
+};
+
+export const signupUser = async (
+    email: string,
+    firstName: string,
+    lastName: string,
+    password: string
+) => {
+    const response = await fetcher(SIGNUP_API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email,
+            firstName,
+            lastName,
+            password,
+            // need to update
+            phone: "",
+            address: {
+                addressLine: "",
+                city: "",
+                country: "",
+                phone: "",
+            },
+        }),
+    });
+
+    if (response.status === "success") return response.data;
+    throw new Error(response.message);
 };
